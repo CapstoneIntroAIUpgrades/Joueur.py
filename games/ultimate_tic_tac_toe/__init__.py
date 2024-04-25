a84de23b97de2b11f12475297d762349c110bc54
Charlie H <coheil3677@gmail.com>
2024-04-25 00:59:12 -0500

    ultimate tic tac toe

---
+++ games/ultimate_tic_tac_toe/__init__.py
@@ -0,0 +1,11 @@
+# DO NOT MODIFY THESE IMPORTS
+from games.ultimate_tic_tac_toe.ai import AI
+from games.ultimate_tic_tac_toe.game import Game
+from games.ultimate_tic_tac_toe.game_object import GameObject
+from games.ultimate_tic_tac_toe.player import Player
+
+game_version = '50e7a74ecd23f8e98bbe235fc2aa7db662a607ebdeb59ad3e5a4213cff4f8a43'
+
+# <<-- Creer-Merge: init -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+# if you need to initialize this module with custom logic do so here
+# <<-- /Creer-Merge: init -->>
